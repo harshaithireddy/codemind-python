bced69a01bc3ac18e37d0b1a25e0bcd585229a80
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2023-05-08 16:01:43 +0530

    n = int(input()) a = list(map(int, input().split())) num = n // 2
    if n % 2 == 0:
        sum1, sum2 = 0, 0
        for i in range(num):
            sum1 += a[i] 
        for i in range(num, n):
            sum2 += a[i]
    else:
        sum1, sum2 = 0, 0
        for i in range(0, num + 1):
            sum1 += i
        for j in range(num, n):
            sum2 += a[j]
    print(abs(sum1 - sum2))

---
+++ difference_between_sum_of_first_half_and_second_half_in_an_array.py
@@ -0,0 +1,17 @@
+n = int(input())
+a = list(map(int, input().split()))
+num = n // 2
+
+if n % 2 == 0:
+    sum1, sum2 = 0, 0
+    for i in range(num):
+        sum1 += a[i] 
+    for i in range(num, n):
+        sum2 += a[i]
+else:
+    sum1, sum2 = 0, 0
+    for i in range(0, num + 1):
+        sum1 += i
+    for j in range(num, n):
+        sum2 += a[j]
+print(abs(sum1 - sum2))
