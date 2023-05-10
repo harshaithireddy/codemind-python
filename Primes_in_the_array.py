20339e0f089ba35f6a749c8f065ba3f4375ddebf
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2023-05-10 15:17:27 +0530

    def isPrime(n):     if n < 2:         return False     for i in range(2, int(n ** 0.5) + 1):         if n % i == 0:             return False     return True
    n = int(input())
    arr = map(int, input().split())
    cnt = 0
    for i in arr:
        if isPrime(i):
            cnt += 1
    print(cnt)

---
+++ Primes_in_the_array.py
@@ -0,0 +1,15 @@
+def isPrime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+n = int(input())
+arr = map(int, input().split())
+cnt = 0
+for i in arr:
+    if isPrime(i):
+        cnt += 1
+print(cnt)
