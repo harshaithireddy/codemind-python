af5f98d1f896261b002de3090b1e41fc0273939f
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2023-05-25 21:36:13 +0530

    def isPrime(n):     if n <= 1:         return False     else:         for i in range(2, int(n ** 0.5) + 1):             if n % i == 0:                 return False     return True
    def isMegaPrime(n):
        if not isPrime(n):
            return False
        for digit in str(n):
            if not isPrime(int(digit)):
                return False
        return True
        
        
    n = int(input())
    if isMegaPrime(n):
        print("Mega Prime")
    else:
        print("Not Mega Prime")

--- mega_prime.py
+++ mega_prime.py
@@ -1,23 +1,23 @@
 def isPrime(n):
-    if n < 2:
+    if n <= 1:
         return False
-    for i in range (2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
+    else:
+        for i in range(2, int(n ** 0.5) + 1):
+            if n % i == 0:
+                return False
     return True
 
-def megaPrime(n):
-    str_n = str(n)
-    cnt = 0
-    for i in range(len(str_n)):
-        if isPrime(int(str_n[i])):
-            cnt += 1
-    if cnt == len(str_n):
-        return True
-    return False
-
+def isMegaPrime(n):
+    if not isPrime(n):
+        return False
+    for digit in str(n):
+        if not isPrime(int(digit)):
+            return False
+    return True
+    
+    
 n = int(input())
-if isPrime(n) and megaPrime(n):
+if isMegaPrime(n):
     print("Mega Prime")
 else:
     print("Not Mega Prime")
