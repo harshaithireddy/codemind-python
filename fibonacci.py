a2220744267a29a9f8e0960c0a03c81bdd060acc
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2023-05-26 18:26:42 +0530

    def fibonacci_series(n):     fib_series = []     a, b = 0, 1
        for _ in range(n):
            fib_series.append(a)
            a, b = b, a + b
            
        return fib_series
    
    
    n = int(input())
    print(*fibonacci_series(n))

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+def fibonacci_series(n):
+    fib_series = []
+    a, b = 0, 1
+
+    for _ in range(n):
+        fib_series.append(a)
+        a, b = b, a + b
+        
+    return fib_series
+
+
+n = int(input())
+print(*fibonacci_series(n))
