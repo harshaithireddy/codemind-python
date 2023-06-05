f9a7c91f7ed3ced56bce7b23e7cac434c9258598
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2023-06-05 20:51:37 +0530

    def isprime(n):     cnt = 0     for i in range (1, n+1) :         if n % i == 0:             cnt += 1     return cnt n = int(input()) if isprime(n) == 2:     print("prime") else:     print("not a prime")

--- prime_number.py
+++ prime_number.py
@@ -1,14 +1,11 @@
-def isPrime(n):
-    if n <= 1:
-        return False
-    else:
-        for i in range(2, int(n ** 0.5) + 1):
-            if n % i == 0:
-                return False
-    return True
-    
+def isprime(n):
+    cnt = 0
+    for i in range (1, n+1) :
+        if n % i == 0:
+            cnt += 1
+    return cnt
 n = int(input())
-if isPrime(n):
+if isprime(n) == 2:
     print("prime")
 else:
     print("not a prime")
