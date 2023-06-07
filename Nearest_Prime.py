08f7cdce12244ec2135b633f901b149c2d48688a
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2023-06-07 21:55:32 +0530

    def isPrime(n):     if n < 2:         return False     for i in range (2, int(n ** 0.5) + 1):         if n % i == 0:             return False     return True
    def nearestPrime(n):
        if isPrime(n):
            return n
        i = 1 
        while True:
            if isPrime(n - i):
                return n - i 
            elif isPrime(n + i):
                return n + i
            i += 1 
                
    t = int(input())
    for i in range (t):
        n = int(input())
        res = nearestPrime(n) 
        print(res)

--- Nearest_Prime.py
+++ Nearest_Prime.py
@@ -5,20 +5,20 @@ def isPrime(n):
         if n % i == 0:
             return False
     return True
+    
 def nearestPrime(n):
     if isPrime(n):
         return n
-    i = 1
+    i = 1 
     while True:
         if isPrime(n - i):
-            return n - i
+            return n - i 
         elif isPrime(n + i):
             return n + i
-        i += 1
-
-
+        i += 1 
+            
 t = int(input())
 for i in range (t):
     n = int(input())
-    res = nearestPrime(n)
+    res = nearestPrime(n) 
     print(res)
